9cbc06fe4a3a43a11860caf665092ec3985c279f
Siyanda-Mzam <dot.netntoz@gmail.com>
2017-12-29 11:54:25 +0200

    Removed deprecated packages. Added loader suffix for babel loaders and made state immutable

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import DevTools from './DevTools';
 
 export default class App extends Component {
--- app/reducers/counter.js
+++ app/reducers/counter.js
@@ -1,11 +1,12 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
 
 export default function counter(state = 0, action) {
+	let newState = state ;
   switch (action.type) {
     case INCREMENT_COUNTER:
-      return state + 1;
+      return newState + 1;
     case DECREMENT_COUNTER:
-      return state - 1;
+      return newState - 1;
     default:
       return state;
   }
